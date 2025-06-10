1c42cf6c4077a34530bdce9d6aad90d5bd804015
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2025-06-10 19:16:53 +0200

    adds migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250610_171640 from './20250610_171640';
+
+export const migrations = [
+  {
+    up: migration_20250610_171640.up,
+    down: migration_20250610_171640.down,
+    name: '20250610_171640'
+  },
+];
