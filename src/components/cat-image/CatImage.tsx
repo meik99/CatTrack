09d71ffedc00647a76c2876e0176c2c91fa8f918
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2025-06-10 20:29:48 +0200

    implements picture upload

--- src/components/cat-image/CatImage.tsx
+++ src/components/cat-image/CatImage.tsx
@@ -1,13 +1,45 @@
-import { Cat, Media } from "@/payload-types"
-import Image from "next/image"
+'use client'
 
-export const CatImage = async({cat, size, className} : {cat: Cat, size?: number, className?: string}) => {
+import { Cat, Media } from '@/payload-types'
+import Image from 'next/image'
+import { uploadImageForCat } from './action'
+
+export function CatImage({
+  cat,
+  size,
+  className,
+}: {
+  cat: Cat
+  size?: number
+  className?: string
+}) {
   if (!size) {
     size = 256
   }
-  
+
   return (
-    <Image src={getImageUrl(cat.images)} alt="cat image" width={size} height={size} className={className}></Image>
+    <div className={`${className} relative group`}>
+      <Image
+        src={getImageUrl(cat.images)}
+        alt="cat image"
+        width={size}
+        height={size}
+        className={className}
+      ></Image>
+
+      <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+
+      <div className="absolute inset-0 flex items-center justify-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer">
+        <label htmlFor="files" className="button">
+          Select Image
+        </label>
+        <input
+          id="files"
+          type="file"
+          onChange={(event) => uploadImageForCat({ cat, images: event.target.files })}
+        ></input>
+      </div>
+    </div>
   )
 }
 
